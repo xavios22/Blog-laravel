7ad4c498c09a6ddf5368f94b8022e9d6d8a11b44
Xavier <xavier@MacBook-Air-de-Xavier.local>
2020-02-18 11:34:54 +0100

    First commit - instalation laravel - intallation argon dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.headers.cards')
+    
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
