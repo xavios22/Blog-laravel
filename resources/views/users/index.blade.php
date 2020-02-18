5fabb5cab86907e8815bbfca23e7220f5be2a0eb
Xavier <xavier@MacBook-Air-de-Xavier.local>
2020-02-18 16:25:37 +0100

    modifications in the blade create a new user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['title' => __('User Management')])
 
 @section('content')
-    @include('layouts.headers.cards')
+      @include('users.partials.header', ['title' => __('Utilisateurs')])  
 
     <div class="container-fluid mt--7">
         <div class="row">
@@ -10,10 +10,10 @@
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">{{ __('Users') }}</h3>
+                                <h3 class="mb-0">{{ __('Utilisateurs') }}</h3>
                             </div>
                             <div class="col-4 text-right">
-                                <a href="{{ route('user.create') }}" class="btn btn-sm btn-primary">{{ __('Add user') }}</a>
+                                <a href="{{ route('user.create') }}" class="btn btn-sm btn-primary">{{ __('Ajouter utilisateur') }}</a>
                             </div>
                         </div>
                     </div>
@@ -33,9 +33,9 @@
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
-                                    <th scope="col">{{ __('Name') }}</th>
+                                    <th scope="col">{{ __('Nom') }}</th>
                                     <th scope="col">{{ __('Email') }}</th>
-                                    <th scope="col">{{ __('Creation Date') }}</th>
+                                    <th scope="col">{{ __('Date de création') }}</th>
                                     <th scope="col"></th>
                                 </tr>
                             </thead>
@@ -59,7 +59,7 @@
                                                             @method('delete')
                                                             
                                                             <a class="dropdown-item" href="{{ route('user.edit', $user) }}">{{ __('Edit') }}</a>
-                                                            <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this user?") }}') ? this.parentElement.submit() : ''">
+                                                            <button type="button" class="dropdown-item" onclick="confirm('{{ __("Êtes-vous sûr de vouloir supprimer cet utilisateur ?") }}') ? this.parentElement.submit() : ''">
                                                                 {{ __('Delete') }}
                                                             </button>
                                                         </form>    
@@ -82,7 +82,5 @@
                 </div>
             </div>
         </div>
-            
-        @include('layouts.footers.auth')
     </div>
 @endsection
