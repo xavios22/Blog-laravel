c1428cbbc614aba13b2165d7a9ec5287ca6bac4e
Xavier <xavier@MacBook-Air-de-Xavier.local>
2020-02-19 12:24:32 +0100

    tables has been database created comments,categories,posts

---
+++ database/migrations/2020_02_19_112115_create_coments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coments', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->string('message');
+            $table->string('e-mail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coments');
+    }
+}
